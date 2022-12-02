93dfda256c36dc98b46b33b5d826d7f084b88ac6
esperancekariuki <kariukiesperance@gmail.com>
2022-12-02 09:55:18 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance<42){
+        return(42-distance)
+    }
+    else{
+        return (distance-42)
+}    
+}
+function distanceFromHqInFeet(istance){
+    if(distance<42){
+        return(42-distance)*264
+    }
+    else{
+        return(distance-42)*264
+    }
+}
+function distanceTravelledInFeet(start,stop){
+    if(stop>start){
+        return((stop-start)*264)
+    }
+    else{
+        return((start-stop)*264)
+    }
+}
+function calcuatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if(distance<=400){
+        return 0;
+    }
+    if(distance>=400 && distance<=2000){
+        return(distance-400)*0.02;
+    }
+    else if(distance>=2000 && distance<=2500){
+        return 25;
+    }
+    else if(distance>2500){
+        return "cannot travel that far"
+    }
+}
