365e440876984673b6464710c11ac52b6f3db561
esperancekariuki <kariukiesperance@gmail.com>
2022-12-09 07:54:12 +0300

    RESUBMITTING ASSIGNMENT

--- index.js
+++ index.js
@@ -38,3 +38,15 @@ function calcuatesFarePrice(start,destination){
         return "cannot travel that far"
     }
 }
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
